cb910faf5126c70b1b13bfdcdd878ca6e4128792
cristian Algar <cristianfmorac@gmail.com>
2025-07-08 12:25:32 -0500

    credential sonarqube

--- vars/staticCodeAnalysis.groovy
+++ vars/staticCodeAnalysis.groovy
@@ -1,6 +1,6 @@
-def call(){
+def call(credentialsId){
 
-    withSonarQubeEnv(credentialsId: 'TOKEN_SONAR') {
+    withSonarQubeEnv(credentialsId: credentialsId) {
         sh 'mvn clean package sonar:sonar'
     }
 }
